c1b7cbfc43ed98bec588ccded31d42a569250783
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2023-01-11 20:33:06 -0800

    added second parallel

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ YP=$(date -u --date="1 day ago" +%y)
 DOY=$(date -u +%j)
 DOYP=$(date -u --date="1 day ago" +%j)
 
-lftp -d -u anonymous,admin@comma.ai -e 'set ftp:ssl-force true' -e "mirror --no-perms --no-umask --parallel=4 --file=/gnss/data/hourly/${YYYY}/${DOY}/hour${DOY}0.${YY}[gn].gz --target=./${YYYY}/${DOY}/; mirror --no-perms --no-umask --file=/gnss/data/hourly/${YYYP}/${DOYP}/hour${DOYP}0.${YP}[gn].gz --target=./${YYYP}/${DOYP}/;exit" gdc.cddis.eosdis.nasa.gov
+lftp -d -u anonymous,admin@comma.ai -e 'set ftp:ssl-force true' -e "mirror --no-perms --no-umask --parallel=4 --file=/gnss/data/hourly/${YYYY}/${DOY}/hour${DOY}0.${YY}[gn].gz --target=./${YYYY}/${DOY}/; mirror --no-perms --no-umask --parallel=4 --file=/gnss/data/hourly/${YYYP}/${DOYP}/hour${DOYP}0.${YP}[gn].gz --target=./${YYYP}/${DOYP}/;exit" gdc.cddis.eosdis.nasa.gov
 
