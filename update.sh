47eb0694839ee2a86c683719c3252c7f087203d4
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2023-01-11 17:34:27 -0800

    cleanUP

--- update.sh
+++ update.sh
@@ -7,30 +7,5 @@ YP=$(date -u --date="1 day ago" +%y)
 DOY=$(date -u +%j)
 DOYP=$(date -u --date="1 day ago" +%j)
 
-RDOY="[0-9][0-9][0-9]"
-RYY="[0-9][0-9]"
+lftp -d -u anonymous,admin@comma.ai -e 'set ftp:ssl-force true' -e "mirror --no-perms --no-umask --parallel=4 --file=/gnss/data/hourly/${YYYY}/${DOY}/hour${DOY}0.${YY}[gn].gz --target=./${YYYY}/${DOY}/; mirror --no-perms --no-umask --file=/gnss/data/hourly/${YYYP}/${DOYP}/hour${DOYP}0.${YP}[gn].gz --target=./${YYYP}/${DOYP}/;exit" gdc.cddis.eosdis.nasa.gov
 
-
-lftp -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=4 --directory=/gnss/data/hourly/${YYYY}/${DOY}/ --directory=/gnss/data/hourly/${YYYP}/${DOYP}/ --include-glob=hour${RDOY}0.${RYY}[ng].gz --target-directory=./;exit" gdc.cddis.eosdis.nasa.gov
-
-
-# check year folders exist
-if [ ! -d ${YYYY} ]; then
-  mkdir ${YYYY}
-fi
-
-if [ ! -d ${YYYP} ]; then
-  mkdir ${YYYP}
-fi
-
-# move into correct folders
-if [ ${YYYP} -ne ${YYYY} ]; then
-  cp -r ${DOYP} ${YYYP}/
-  cp -r ${DOY} ${YYYY}/
-else
-  cp -r ${DOYP} ${YYYY}/
-  cp -r ${DOY} ${YYYY}/
-fi
-
-rm -rf ${DOYP}
-rm -rf ${DOY}
